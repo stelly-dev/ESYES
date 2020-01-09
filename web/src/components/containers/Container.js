63e15af36a943554aa81dd46777808a23652dd8d
cantorsdustbunnies <kclarencestratton@gmail.com>
2020-01-09 15:36:21 -0700

    reworked container component

--- web/src/components/containers/Container.js
+++ web/src/components/containers/Container.js
@@ -1,15 +1,10 @@
 import styled from "styled-components"
 
 const Container = styled.div`
-  max-width: ${props => props.theme.sizes.maxWidth[0]};
+  max-width: 1674px;
+  width: 100%;
   margin: 0 auto;
-  display: flex;
-  flex-direction: ${props => (props.col ? "column" : "row")};
-  height: 100%;
-  align-items: center;
-  @media only screen and (min-width: 1024px) {
-    max-width: ${props => props.theme.sizes.maxWidth[1]};
-  }
+  padding: 0 1rem;
 `
 
 export default Container
