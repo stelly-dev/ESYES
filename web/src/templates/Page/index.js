2dfd492bc992872748f254daa8e3fe12e59a7634
cantorsdustbunnies <kclarencestratton@gmail.com>
2020-01-28 03:48:19 -0700

    fixed resolution of documents with deep references
    Note to self:
    
    When encountering a situation where it isn't clear how to get the data
    from sanity, check to see if the document you are referencing is too
    deep in the tree for the resolvers.
    
    In this case I just doubled the maxDepth to 10, but it's possible this
    may not suffice for everything.

--- web/src/templates/Page/index.js
+++ web/src/templates/Page/index.js
@@ -1,43 +1,56 @@
 import React from "react"
+import Img from "gatsby-image"
 import { graphql } from "gatsby"
 import Layout from "../../Layout"
 import Hero from "../../components/sections/Hero/"
-import { mapHeroToProps } from "../../utils/mapToProps"
+import HomePageTextSection from "../../components/sections/HomePageTextSection/"
+import {
+  mapHeroToProps,
+  mapHomePageTextSectionToProps,
+} from "../../utils/mapToProps"
 
 export const query = graphql`
   query RootPageTemplateQuery($id: String!) {
     page: sanityPage(id: { eq: $id }) {
       id
       pageName
-      _rawContent(resolveReferences: { maxDepth: 5 })
+      _rawContent(resolveReferences: { maxDepth: 10 })
     }
   }
 `
 
 const PageTemplate = props => {
   const { data, errors } = props
-  const page = data && data.page
 
   const content = data && data.page && data.page["_rawContent"]
   return (
     <Layout>
-      {page && page.pageName}
       {errors ? <pre>{JSON.stringify(errors, null, 2)}</pre> : null}
       {content &&
         content.map((section, i) => {
+          console.log(section)
           const { _type } = section
           switch (_type) {
             case "pageHero":
-              return <Hero {...mapHeroToProps(section)} />
+              return <Hero {...mapHeroToProps(section)} key={section._key} />
+            case "homePageText":
+              return (
+                <HomePageTextSection
+                  {...mapHomePageTextSectionToProps(section)}
+                  key={section._key}
+                />
+              )
             default:
               return (
-                <div>
-                  <pre>{JSON.stringify(_type, null, 2)}</pre>
+                <div key={Math.random()}>
+                  <span style={{ color: "red" }}>
+                    No resolver for type "{_type}" found.
+                  </span>
                 </div>
               )
           }
         })}
-      {content && <pre>{JSON.stringify(content, null, 2)}</pre>}
+      {/* {content && <pre>{JSON.stringify(content, null, 2)}</pre>} */}
     </Layout>
   )
 }
