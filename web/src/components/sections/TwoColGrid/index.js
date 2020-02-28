5bf0ed19e7b1796c3988c62ce83f8f643f3a17d2
cantorsdustbunnies <kclarencestratton@gmail.com>
2020-02-28 00:48:49 -0700

    Two Col Grid will replace ImageLeftSection

---
+++ web/src/components/sections/TwoColGrid/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import styled from "styled-components"
+import Grid from "../../containers/Grid"
+import Img from "gatsby-image"
+import { getFluidGatsbyImage } from "gatsby-source-sanity"
+import clientConfig from "../../../../client-config"
+import Block from "@sanity/block-content-to-react"
+
+const TwoColGrid = props => {
+  console.log(props)
+  console.log(clientConfig.sanity)
+  return (
+    <Grid.Container>
+      <Grid.Row display={[null, null, "flex"]}>
+        <Grid.Col flexBasis={["50%"]} marginRight={[null, null, "2rem"]}>
+          <Img
+            fluid={getFluidGatsbyImage(
+              props.image,
+              { maxWidth: 1200 },
+              clientConfig.sanity
+            )}
+            alt={props.altText}
+          />
+        </Grid.Col>
+        <Grid.Col flexBasis={["50%"]}>
+          <Block blocks={props.features} />
+        </Grid.Col>
+      </Grid.Row>
+    </Grid.Container>
+  )
+}
+
+export default TwoColGrid
