87010fe0240bc6541cd886e5f1bffd7b3d0fb433
cantorsdustbunnies <kclarencestratton@gmail.com>
2020-02-29 08:42:37 -0700

    updated imports to include prop-types

--- web/src/components/sections/ReviewSection/Counter.js
+++ web/src/components/sections/ReviewSection/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import PropTypes from "prop-types"
 import CountUp from "react-countup"
 import VizSensor from "react-visibility-sensor"
 import { CounterBackground } from "./styled.js"
