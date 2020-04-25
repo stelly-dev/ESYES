0a7c0e9244d8e1a8022e17c45c10fdab95f8f353
cantorsdustbunnies <kclarencestratton@gmail.com>
2020-04-25 16:50:54 -0600

    created key/value pairs for mapping over section componenets

---
+++ web/src/templates/Page/appSections.js
@@ -0,0 +1,58 @@
+export const appSections = {
+  fileLink: { sectionFileName: "FileLink", mapFuncName: "mapFileLinkToProps" },
+  listBanner: {
+    sectionFileName: "BannerList",
+    mapFuncName: "mapBannerListToProps",
+  },
+  blockImage: {
+    sectionFileName: "BlockImage",
+    mapFuncName: "mapBlockImageToProps",
+  },
+  contractorAccordion: {
+    sectionFileName: "ContractorAccordion",
+    mapFuncName: "mapContractorAccordionToProps",
+  },
+  banner: { sectionFileName: "Banner", mapFuncName: "mapBannerToProps" },
+  block: { sectionFileName: "Block" },
+  blockButton: {
+    sectionFileName: "BlockButton",
+    mapFuncName: "mapBlockButtonToProps",
+  },
+  circleCTAList: {
+    sectionFileName: "CircleCTASection",
+    mapFuncName: "mapCircleCTASectionToProps",
+  },
+  imageLeftSection: {
+    sectionFileName: "TwoColGrid",
+    mapFuncName: "mapTwoColGridToProps",
+  },
+  pageHero: { sectionFileName: "Hero", mapFuncName: "mapHeroToProps" },
+  reviewSection: {
+    sectionFileName: "ReviewSection",
+    mapFuncName: "mapReviewSectionToProps",
+  },
+  simpleCTA: {
+    sectionFileName: "SimpleCTA",
+    mapFuncName: "mapSimpleCTAToProps",
+  },
+  staffList: {
+    sectionFileName: "StaffList",
+    mapFuncName: "mapStaffListToProps",
+  },
+  twoColumn: {
+    sectionFileName: "TwoColumn",
+    mapFuncName: "mapTwoColumnToProps",
+  },
+  vimeo: {
+    sectionFileName: "VimeoSection",
+    mapFuncName: "mapVimeoSectionToProps",
+  },
+  youtube: {
+    sectionFileName: "YoutubeSection",
+    mapFuncName: "mapYoutubeSectionToProps",
+  },
+  homePageText: {
+    sectionFileName: "HomePageTextSection",
+    mapFuncName: "mapHomePageTextSectionToProps",
+  },
+}
