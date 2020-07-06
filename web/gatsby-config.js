9d4f9b395fc9bba3144aed8a970c7e0635e3d751
cantorsdustbunnies <kclarencestratton@gmail.com>
2020-07-06 08:57:34 -0600

    added robots.txt with guards against netlify testing-builds

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,17 +1,53 @@
 require("dotenv").config()
-
+let placeholder = Array.from({ length: 1000 })
+  .map((_, index) => "#")
+  .join("")
+console.log(placeholder, process.env.NODE_ENV, placeholder)
 const path = require("path")
 // const blocksToText = require("./utils/blocksToText.js")
+
+// This prevents crawling of our netlify previews / branch deploys
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://energysmartyes.com",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === "production"
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
 
 module.exports = {
   siteMetadata: {
     title: `Energy Smart`,
-    siteUrl: `https://energysmartyes.com`,
+    siteUrl,
   },
   plugins: [
     // `gatsby-plugin-eslint`,
     `gatsby-plugin-preact`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: `*`, allow: [`/`] }],
+            sitemap: "https://energysmartyes.com/sitemap.xml",
+            host: "https://energysmartyes.com",
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: `*`, disallow: [`/`] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgen: `*`, disallow: [`/`] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -25,6 +61,8 @@ module.exports = {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
         token: process.env.SANITY_TOKEN,
+        watchMode: process.env.NODE_ENV === "development" ? true : false,
+        overlayDrafts: process.env.NODE_ENV === "development" ? true : false,
       },
     },
     "gatsby-source-sanity-transform-images",
